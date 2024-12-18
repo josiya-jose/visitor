e334d3cde76226ee0996957225cf71a683b99fd6
josiya-jose <josiyajosenayathode@gmail.com>
2024-12-18 15:46:47 +0530

    menu created

--- app/src/main/java/com/example/visitor/MainActivity.java
+++ app/src/main/java/com/example/visitor/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.visitor;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1,btn2,btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btn1=(Button) findViewById(R.id.add);
+        btn2=(Button) findViewById(R.id.view);
+        btn3=(Button) findViewById(R.id.search);
+
+
     }
 }
